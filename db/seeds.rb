41fbf39b77bb69625d938ad92b3c555d5f5bd53a
Glenn <waltandmartha@gmail.com>
2018-04-20 12:05:01 +1000

    add env and devise methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!({
+    email: ENV.fetch('USER_EMAIL'),
+    password: ENV.fetch('USER_PASSWORD')
+}) { |u| p u.encrypted_password }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have matching email' do 
+    user = User.new
+    user.email = ENV.fetch('USER_EMAIL')
+    expect(user.email).to eq('glenn@example.com')
+  end 
 end
